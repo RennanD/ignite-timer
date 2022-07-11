0e3871684d1515143d2c8a3996295d890cecdc41
RennanD <rplaykl@gmail.com>
2022-07-10 22:16:35 -0300

    chore: configure react-hook-form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 import { Play } from 'phosphor-react'
 
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   HomeContainer,
   CountdonwContainer,
@@ -10,10 +14,34 @@ import {
   MinutesAmountInput,
 } from './styles'
 
+const timerValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'A tarefa de ter no mínimo 5 minutos')
+    .max(60, 'A tarefa de ter no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof timerValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(timerValidationSchema),
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+
+    reset()
+  }
+
+  const task = watch('task')
+
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -21,6 +49,7 @@ export function Home() {
             type="text"
             placeholder="Dê um nome para seu o seu projeto"
             list="task-suggestions"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -34,10 +63,11 @@ export function Home() {
           <MinutesAmountInput
             type="number"
             step={5}
-            min={5}
-            max={60}
+            // min={5}
+            // max={60}
             id="minutesAmount"
             placeholder="00"
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -53,7 +83,7 @@ export function Home() {
           <span>0</span>
         </CountdonwContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
